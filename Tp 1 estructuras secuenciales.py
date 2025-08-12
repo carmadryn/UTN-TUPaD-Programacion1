9f2749e27edd1a396c5679cea28373b0d8c00b83
carmadryn <cstorniolo@gmail.com>
2025-08-12 15:35:58 -0300

    subo práctica unidad 1

---
+++ Tp 1 estructuras secuenciales.py
@@ -0,0 +1,58 @@
+# Escribir programa que diga Hola Mundo !
+# print ("Hola munndo !")
+
+#escribir programa consultando nombre y luego saludarlo
+#nombre= input("cuál es su nombre?")
+#print (f"Hola {nombre}")
+
+# Pedir al usuario nombre, apellido, edad y lugar de residencia y luego brindar esos datos en oración
+#nombre= input("cuál es su nombre?")
+#apellido=input ("cuál es su apellido?")
+#edad=int(input("cuál es su edad?"))
+#residencia= input("¿Dónde vivís?")
+#print(f"soy {nombre} {apellido} , tengo {edad} años y vivo en {residencia}.")
+
+#Pedir radio de un círculo e imprimir su área y perímetro
+#radio= int(input("dime radio de un circulo"))
+#area= 3,14 * (radio*radio)
+#perimetro= 2 * 3,14* radio
+#print(f" El áreo del círculo es de {area} y su perímetro {perimetro}")
+
+#Pedir cant de segundos y convertir a horas
+#segundos= int(input("dime una cantidad de segundos"))
+#horas= segundos/3600
+#print(f"Estos {segundos} segundos son {horas} horas")
+
+#pedir un número y crear tabla de multiplicación
+#numero=int(input("Dime un numero y creare su tabla de multiplicar: "))
+#tabla2= numero*2
+#tabla3=numero*3
+#tabla4=numero*4
+#print (f"la tabla de multiplicar de {numero} es: {numero}x2= {tabla2}, {numero}x3= {tabla3}, {numero} x 4= {tabla4}")
+
+# Pedir dos numero diferentes al 0 y sumar, restar multiplicar
+#num1=int(input("dime un numero: "))
+#num2=int(input("dime otro numero: "))
+#suma=num1+num2
+#resta=num1-num2
+#mult=num1*num2
+#print (f"El resultado de la suma de ambos números es :{suma}, la resta: {resta} y la multiplicación : {mult}")
+
+#pedir altura y peso y sacar IMC
+#altura=float(input("DIme tu altura en mt : "))
+#peso=float(input("DIme tu peso en kg : "))
+#imc= peso / (altura*altura)
+#print (f" Su imc es {imc}")
+
+#pedir temp en grados celsius y pasas su euivalente en fahrenheit
+#celcius=int(input("dime la temperatura: "))
+#fahr= celcius +32
+#print (f"La temperatura en fahrenheit es : {fahr}")
+
+# Pedir 3 numeros y dar el promedio
+num1 =int(input("dime numero 1 :"))
+num2 =int(input("dime numero 2 :"))
+num3 =int(input("dime numero 3 :"))
+promedio= (num1+num2+num3)/3
+
+print (f"El promedio de los numeros dados es de : {promedio}")
